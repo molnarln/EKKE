7ac4d94653b976f977fe4c58d2f7ed8f9def7653
Molnár László <laszlo.molnar25@gmail.com>
2024-11-08 20:40:40 +0100

    Add statusbar practice

---
+++ MagProg1/GYAK/StatusBar_practice/StatusBar_practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StatusBar_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int currentCursorPosition = 0;
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    //Need to consume the keypress with ReadKey to clear the KeyAvailable value!
+                    ConsoleKeyInfo cki = Console.ReadKey(true);
+                    //if (cki.Key == ConsoleKey.Spacebar)
+                    //{
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("     ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.SetCursorPosition(currentCursorPosition + 6, 0);
+                        currentCursorPosition += 6;
+                    //}
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
